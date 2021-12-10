d6c544f0d595085d9ea7c9d54b01d8db1137398a
Safwan Shaheer <devorein00@gmail.com>
2021-12-10 21:04:04 +0600

    Connected to database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'test'
+});
 
 const app = express();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`);
+  connection.connect((err) => {
+    if (err) {
+      console.error(`Error connecting to database\n${err.message}`)
+    } else {
+      console.log(`Connected to database`);
+    }
+  })
 })
