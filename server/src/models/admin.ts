8bd884322f488de41e84af5f67fbe72643db8ccd
Safwan Shaheer <devorein00@gmail.com>
2021-12-12 17:50:13 +0600

    Added login as admin feature

--- server/src/models/admin.ts
+++ server/src/models/admin.ts
@@ -4,7 +4,7 @@ import query from '../utils/query';
 const AdminModel = {
 	async findByEmail(email: string) {
 		const queryResponse = (await query(`
-      SELECT * FROM police where email = "${email}";
+      SELECT * FROM admin where email = "${email}";
     `)) as Array<IAdmin & { password: string }>;
 		if (queryResponse.length === 0) {
 			return null;
