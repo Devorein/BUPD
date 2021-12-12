284c34abc340016629ffb264f63b1ad9ce4aa76d
Safwan Shaheer <devorein00@gmail.com>
2021-12-12 18:16:30 +0600

    Fixed res.json being called more than once

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -19,68 +19,68 @@ export default {
 				status: 'error',
 				message: 'Email is required',
 			});
-		}
-
-		const payload = req.body;
-		if (payload.as === 'police') {
-			const queryResponse = await PoliceModel.findByEmail(payload.email);
-			if (!queryResponse) {
-				res.json({
-					status: 'error',
-					message: 'No police exists with that email',
-				});
-			} else {
-				const isCorrectPassword = await argon2.verify(queryResponse.password, payload.password);
-				if (!isCorrectPassword) {
+		} else {
+			const payload = req.body;
+			if (payload.as === 'police') {
+				const queryResponse = await PoliceModel.findByEmail(payload.email);
+				if (!queryResponse) {
 					res.json({
 						status: 'error',
-						message: 'Incorrect password',
+						message: 'No police exists with that email',
 					});
 				} else {
-					const jwtToken = signToken({
-						type: 'police',
-						nid: queryResponse.nid,
-						email: queryResponse.email,
-					});
-					res.json({
-						status: 'success',
-						data: {
-							email: queryResponse.email,
-							name: queryResponse.name,
+					const isCorrectPassword = await argon2.verify(queryResponse.password, payload.password);
+					if (!isCorrectPassword) {
+						res.json({
+							status: 'error',
+							message: 'Incorrect password',
+						});
+					} else {
+						const jwtToken = signToken({
+							type: 'police',
 							nid: queryResponse.nid,
-							token: jwtToken,
-						},
-					});
+							email: queryResponse.email,
+						});
+						res.json({
+							status: 'success',
+							data: {
+								email: queryResponse.email,
+								name: queryResponse.name,
+								nid: queryResponse.nid,
+								token: jwtToken,
+							},
+						});
+					}
 				}
-			}
-		} else if (payload.as === 'admin') {
-			const queryResponse = await AdminModel.findByEmail(payload.email);
-			if (!queryResponse) {
-				res.json({
-					status: 'error',
-					message: 'No admin exists with that email',
-				});
-			} else {
-				const isCorrectPassword = await argon2.verify(queryResponse.password, payload.password);
-				if (!isCorrectPassword) {
+			} else if (payload.as === 'admin') {
+				const queryResponse = await AdminModel.findByEmail(payload.email);
+				if (!queryResponse) {
 					res.json({
 						status: 'error',
-						message: 'Incorrect password',
+						message: 'No admin exists with that email',
 					});
 				} else {
-					const jwtToken = signToken({
-						type: 'admin',
-						email: queryResponse.email,
-						id: queryResponse.id,
-					});
-					res.json({
-						status: 'success',
-						data: {
+					const isCorrectPassword = await argon2.verify(queryResponse.password, payload.password);
+					if (!isCorrectPassword) {
+						res.json({
+							status: 'error',
+							message: 'Incorrect password',
+						});
+					} else {
+						const jwtToken = signToken({
+							type: 'admin',
 							email: queryResponse.email,
 							id: queryResponse.id,
-							token: jwtToken,
-						},
-					});
+						});
+						res.json({
+							status: 'success',
+							data: {
+								email: queryResponse.email,
+								id: queryResponse.id,
+								token: jwtToken,
+							},
+						});
+					}
 				}
 			}
 		}
@@ -119,44 +119,45 @@ export default {
 				status: 'error',
 				message: 'Invalid email',
 			});
-		}
-		try {
-			const hashedPassword = await argon2.hash(req.body.password, {
-				hashLength: 100,
-				timeCost: 5,
-				salt: Buffer.from(process.env.PASSWORD_SALT!, 'utf-8'),
-			});
-
-			const jwtToken = signToken({
-				type: 'police',
-				nid: req.body.nid,
-				email: req.body.email,
-			});
-
-			const police = await PoliceModel.create({
-				...req.body,
-				password: hashedPassword,
-			});
-
-			res.json({
-				status: 'success',
-				data: {
-					...police,
-					token: jwtToken,
-				},
-			});
-		} catch (err) {
-			if (err.code === 'ER_DUP_ENTRY') {
-				const isDuplicateEmail = err.sqlMessage.includes('email');
-				res.json({
-					status: 'error',
-					message: `A police already exists with this ${isDuplicateEmail ? 'email' : 'NID'}`,
+		} else {
+			try {
+				const hashedPassword = await argon2.hash(req.body.password, {
+					hashLength: 100,
+					timeCost: 5,
+					salt: Buffer.from(process.env.PASSWORD_SALT!, 'utf-8'),
 				});
-			} else {
-				res.json({
-					status: 'error',
-					message: err.message,
+
+				const jwtToken = signToken({
+					type: 'police',
+					nid: req.body.nid,
+					email: req.body.email,
 				});
+
+				const police = await PoliceModel.create({
+					...req.body,
+					password: hashedPassword,
+				});
+
+				res.json({
+					status: 'success',
+					data: {
+						...police,
+						token: jwtToken,
+					},
+				});
+			} catch (err) {
+				if (err.code === 'ER_DUP_ENTRY') {
+					const isDuplicateEmail = err.sqlMessage.includes('email');
+					res.json({
+						status: 'error',
+						message: `A police already exists with this ${isDuplicateEmail ? 'email' : 'NID'}`,
+					});
+				} else {
+					res.json({
+						status: 'error',
+						message: err.message,
+					});
+				}
 			}
 		}
 	},
