147be78708244e339766ed27c2a020de8ef1fbf3
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-12-26 16:50:29 +0600

    FEAT: added removeField test feature

---
+++ server/tests/utils/removeFields.test.ts
@@ -0,0 +1,19 @@
+/// <reference types="@types/jest"/>
+
+import removeFields from '../../src/utils/removeFields';
+
+describe('.removeFields', () => {
+	it(`Should return an object after removing the required key, val`, () => {
+		expect(
+			removeFields(
+				{
+					field1: 'value1',
+					field2: 'value2',
+				},
+				['field2' as any]
+			)
+		).toStrictEqual({
+			field1: 'value1',
+		});
+	});
+});
