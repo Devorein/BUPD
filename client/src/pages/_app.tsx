1100de54dbc5dc26604387199590b1d89179529a
Safwan Shaheer <devorein00@gmail.com>
2021-12-28 19:18:54 +0600

    Added MUI, Notistack, React Query contexts

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,32 @@
+import { ThemeProvider } from "@mui/material/styles";
 import { AppProps } from 'next/app';
+import Head from "next/head";
+import { SnackbarProvider } from "notistack";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
 import '../styles/main.css';
+import { createClient, generateTheme } from "../utils";
+
+const generatedTheme = generateTheme();
+const client = createClient()
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ThemeProvider theme={generatedTheme}>
+    <Head>
+      <title>BUPD</title>
+    </Head>
+    <QueryClientProvider client={client}>
+      <SnackbarProvider maxSnack={4}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </SnackbarProvider>
+    </QueryClientProvider>
+  </ThemeProvider>
 );
 
 export default MyApp;
---
+++ client/src/utils/generateTheme.ts
@@ -0,0 +1,74 @@
+import { grey, red } from '@mui/material/colors';
+import { createTheme, ThemeOptions } from '@mui/material/styles';
+
+export function generateTheme() {
+	const text: {
+		primary: string;
+		secondary: string;
+	} = {
+		primary: grey[900],
+		secondary: grey[800],
+	};
+
+	text.primary = grey[900];
+	text.secondary = grey[700];
+
+	const themeOptions: ThemeOptions = {
+		palette: {
+			error: {
+				main: red[500],
+			},
+			text: {
+				primary: text.primary,
+				secondary: text.secondary,
+			},
+			background: {
+				default: grey[200],
+			},
+			primary: {
+				main: grey[900],
+			},
+		},
+		typography: {
+			fontFamily: 'Poppins',
+			fontSize: 14,
+			body1: {
+				fontWeight: 500,
+				fontSize: '1em',
+			},
+			body2: {
+				fontWeight: 500,
+				fontSize: '0.85em',
+			},
+			subtitle1: {
+				fontSize: '1em',
+				fontWeight: 400,
+				color: text.secondary,
+			},
+			subtitle2: {
+				fontSize: '0.85em',
+				fontWeight: 300,
+				color: text.secondary,
+			},
+			h3: {
+				fontWeight: 900,
+			},
+			h4: {
+				fontWeight: 800,
+			},
+			h5: {
+				fontWeight: 700,
+			},
+			h6: {
+				fontWeight: 600,
+			},
+			caption: {
+				fontSize: '1em',
+				fontWeight: 700,
+			},
+		},
+		components: {},
+	};
+
+	return createTheme(themeOptions);
+}
