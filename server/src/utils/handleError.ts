6b279cb3ad919b5e580d7eec3f3b4d616d04fdbc
xImouto <mohammad.rafid.hamid@gmail.com>
2021-12-28 19:22:22 +0600

    handleError improved

--- server/src/utils/handleError.ts
+++ server/src/utils/handleError.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express';
 import { ErrorApiResponse } from '../types';
 
-const handleError = (res: Response<ErrorApiResponse>, statusCode: number, message: string) => {
-	res.status(statusCode).json({
+const handleError = (res: Response<ErrorApiResponse>, statusCode?: number, message?: string) => {
+	res.status(statusCode === undefined ? 404 : statusCode).json({
 		status: 'error',
-		message,
+		message: message === undefined ? 'Something went wrong, please try again!' : message,
 	});
 };
 
