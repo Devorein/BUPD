cbf7bea8a36ec7a2c5b60228e290725745ec0505
Safwan Shaheer <devorein00@gmail.com>
2021-12-30 11:29:14 +0600

    Fixed vercel script

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -5,6 +5,10 @@ echo "VERCEL_ENV: $VERCEL_ENV"
 if [[ "$VERCEL_ENV" == "production" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
+  cd ../../
+  npm install
+  lerna bootstrap
+  cd ./packages/client
   exit 1;
 else
   # Don't build
