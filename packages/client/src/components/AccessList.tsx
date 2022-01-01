432ca21444f2f7d74a6224b150fc1bfadea9f759
Safwan Shaheer <devorein00@gmail.com>
2022-01-01 23:07:56 +0600

    Attached access update approval with client

--- packages/client/src/components/AccessList.tsx
+++ packages/client/src/components/AccessList.tsx
@@ -1,6 +1,6 @@
 import { IAccess } from "@bupd/types";
 import { Checkbox } from "@mui/material";
-import { useUpdateAccessMutation } from "../api/mutations/useUpdateAccessMutation";
+import { useUpdateAccessMutation, useUpdateAccessMutationCache } from "../api/mutations/useUpdateAccessMutation";
 
 interface AccessListProps {
   accesses: IAccess[]
@@ -9,6 +9,8 @@ interface AccessListProps {
 function AccessListItem(props: { access: IAccess }) {
   const { access } = props;
   const updateAccessMutation = useUpdateAccessMutation(access.access_id);
+  const updateAccessMutationCache = useUpdateAccessMutationCache();
+
   return <div className="items-center flex gap-3 border-2 shadow-md px-5 py-2 rounded-sm justify-between" style={{ borderColor: '#dad8d85e' }}>
     <div className="text-lg font-semibold hover:underline cursor-pointer mr-5">{access.police_nid}</div>
     <div className="mr-5">Requesting <span className="uppercase font-bold text-sm">{access.permission}</span> access</div>
@@ -16,7 +18,7 @@ function AccessListItem(props: { access: IAccess }) {
     <Checkbox color="secondary" checked={Boolean(access.approved)} onChange={() => {
       updateAccessMutation.mutate({
         approved: access.approved === 0 ? 1 : 0
-      })
+      }, updateAccessMutationCache(access.access_id))
     }} />
   </div>
 }
