67e5f9bffb51b1561c99cd5c189ef56958ed4f39
Safwan Shaheer <devorein00@gmail.com>
2022-01-04 19:44:14 +0600

    Added access requests component for polices

--- packages/client/src/components/PoliceHome.tsx
+++ packages/client/src/components/PoliceHome.tsx
@@ -4,6 +4,7 @@ import { Button } from "./Button";
 export function PoliceHome() {
   return <div className="flex w-full flex-col gap-5 h-full items-center justify-center">
     <Button onClick={() => router.push('/victims')} content="View Victims" />
+    <Button onClick={() => router.push('/police/accesses')} content="View My Accesses" />
     <Button onClick={() => router.push('/casefiles')} content="View Casefiles" />
     <Button onClick={() => router.push('/criminals')} content="View Criminals" />
   </div>
--- packages/client/src/pages/accesses.tsx
+++ packages/client/src/pages/accesses.tsx
@@ -1,60 +1,32 @@
-import { GetAccessesPayload, IAccessPopulated, IAccessSort } from "@bupd/types";
-import { useState } from "react";
-import { useGetAccessesQuery } from "../api";
-import { AccessDetails, AccessDetailsProps } from "../components/AccessDetails";
-import { AccessList } from "../components/AccessList";
-import { Paginate } from "../components/Paginate";
-import { accessSortLabelRecord } from "../constants";
-
-const createInitialGetAccessesQuery = (): GetAccessesPayload => ({
-  limit: 10,
-  next: null,
-  sort: ["approved", -1],
-  filter: {
-    approved: [],
-    permission: [],
-    type: [],
-  }
-})
+import { GetAccessesPayload, IAccessPopulated, IAccessSort } from '@bupd/types';
+import { useState } from 'react';
+import { useGetAccessesQuery } from '../api';
+import { AccessDetails, AccessDetailsProps } from '../components/AccessDetails';
+import { AccessList } from '../components/AccessList';
+import { Paginate } from '../components/Paginate';
+import {
+  AccessesFilterGroup,
+  accessSortLabelRecord,
+  createInitialGetAccessesQuery
+} from '../constants';
 
 export default function Accesses() {
-  const [currentDetail, setAccessDetail] = useState<AccessDetailsProps["data"]>(null)
-  return <div className="flex gap-5 h-full">
-    <Paginate<GetAccessesPayload, IAccessSort, IAccessPopulated> filterGroups={[{
-      type: "checkbox_group",
-      props: {
-        items: [
-          ['2', <div key="unapproved">Unapproved</div>],
-          ['1', <div key="approved">Approved</div>],
-          ['0', <div key="disapproved">Disapproved</div>]
-        ],
-        label: "Approval",
-        stateKey: "approved"
-      }
-    }, {
-      type: "checkbox_group",
-      props: {
-        items: [
-          ['case', <div key="case">Case</div>],
-          ['criminal', <div key="criminal">Criminal</div>]
-        ],
-        label: "Type",
-        stateKey: "type"
-      }
-    }, {
-      type: "checkbox_group",
-      props: {
-        items: [
-          ['read', <div key="read">View</div>],
-          ['update', <div key="update">Update</div>],
-          ['delete', <div key="delete">Delete</div>]
-        ],
-        label: "Permission",
-        stateKey: "permission"
-      }
-    }]} dataListComponentFn={(items) => <AccessList accesses={items} setAccessDetail={setAccessDetail} />} clientQueryFn={createInitialGetAccessesQuery} dataFetcher={useGetAccessesQuery} label="Access Requests" sortLabelRecord={accessSortLabelRecord} />
-    <div className="my-2">
-      <AccessDetails data={currentDetail} />
+  const [currentDetail, setAccessDetail] = useState<AccessDetailsProps['data']>(null);
+  return (
+    <div className="flex gap-5 h-full">
+      <Paginate<GetAccessesPayload, IAccessSort, IAccessPopulated>
+        filterGroups={AccessesFilterGroup()}
+        dataListComponentFn={(items) => (
+          <AccessList accesses={items} setAccessDetail={setAccessDetail} />
+        )}
+        clientQueryFn={createInitialGetAccessesQuery}
+        dataFetcher={useGetAccessesQuery}
+        label="Access Requests"
+        sortLabelRecord={accessSortLabelRecord}
+      />
+      <div className="my-2">
+        <AccessDetails data={currentDetail} />
+      </div>
     </div>
-  </div>
+  );
 }
