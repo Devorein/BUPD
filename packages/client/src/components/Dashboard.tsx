72befef0ecf067ad25211c44e0b5f197493c4ec9
Safwan Shaheer <devorein00@gmail.com>
2022-01-06 14:24:23 +0600

    Refactored DashboardItem component

--- packages/client/src/components/Dashboard.tsx
+++ packages/client/src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { PRIORITY_RECORD } from "@bupd/constants";
-import { TCasefilePriority } from "@bupd/types";
 import { Typography } from "@mui/material";
 import router from "next/router";
 import { useGetDashboardQuery } from "../api/queries/useGetDashboardQuery";
@@ -7,9 +6,32 @@ import { useIsAuthenticated } from "../hooks";
 import { Button } from "./Button";
 import { DetailsList } from "./DetailsList";
 
-export function Dashboard() {
-  const currentUser = useIsAuthenticated();
+interface DashboardItemProps {
+  label: string
+  children?: JSX.Element | JSX.Element[]
+  total: number
+}
 
+function DashboardItem(props: DashboardItemProps) {
+  const currentUser = useIsAuthenticated();
+  const { label, total, children } = props;
+  return <div className="shadow-md border-2 p-5 rounded-md flex flex-col justify-between gap-3">
+    <div className="flex flex-col gap-2">
+      <Typography variant="h5" className="uppercase">
+        {label}
+      </Typography>
+      <div>Total: <span className="font-bold">{total}</span></div>
+      {children && <div className="flex gap-10">
+        {children}
+      </div>}
+    </div>
+    <Button style={{
+      width: 'fit-content'
+    }} content={`View ${label.toLowerCase()}`} onClick={() => router.push({ pathname: `${currentUser.type}/${label.toLowerCase()}` })} />
+  </div>
+}
+
+export function Dashboard() {
   const { data: getDashboardQueryData, isLoading: isGetDashboardQueryLoading } = useGetDashboardQuery();
   const dashboardData = getDashboardQueryData?.status === "success" ? getDashboardQueryData.data : null;
 
@@ -26,95 +48,43 @@ export function Dashboard() {
     })
   }
 
-  return dashboardData && !isGetDashboardQueryLoading ? <div className="flex gap-5 w-full h-full">
-    <div className="flex gap-5 flex-col">
-      <div className="shadow-md border-2 p-5 rounded-md flex flex-col flex-grow justify-between">
-        <div className="flex flex-col gap-2">
-          <Typography variant="h5" className="uppercase">
-            Casefiles
-          </Typography>
-          <div>Total: <span className="font-bold">{totalCasefiles}</span></div>
-          <div className="flex gap-10">
-            <div className="flex flex-col gap-1">
-              <Typography variant="h6">Status</Typography>
-              <div>
-                <DetailsList items={Object.entries(dashboardData.casefiles.status)} />
-              </div>
-            </div>
-            <div className="flex flex-col gap-1">
-              <Typography variant="h6">Priority</Typography>
-              <div>
-                <DetailsList items={Object.entries(dashboardData.casefiles.priority).map(([priority, total]) => [PRIORITY_RECORD[priority as unknown as TCasefilePriority], total])} />
-              </div>
-            </div>
+  return dashboardData && !isGetDashboardQueryLoading ? <div className="flex gap-3 w-full h-full">
+    <div className="flex gap-3 flex-col">
+      <DashboardItem label="Casefiles" total={totalCasefiles}>
+        <div className="flex flex-col gap-1">
+          <Typography variant="h6">Status</Typography>
+          <div>
+            <DetailsList items={Object.entries(dashboardData.casefiles.status)} />
           </div>
         </div>
-        <Button content="View casefiles" onClick={() => router.push({ pathname: `${currentUser.type}/casefiles` })} />
-      </div>
+        <div className="flex flex-col gap-1">
+          <Typography variant="h6">Priority</Typography>
+          <div>
+            <DetailsList items={Object.entries(dashboardData.casefiles.priority).map(([priority, total]) => [PRIORITY_RECORD[priority]!, total])} />
+          </div>
+        </div>
+      </DashboardItem>
 
-      <div className="shadow-md border-2 p-5 rounded-md flex flex-col gap-2 justify-between">
-        <div className="flex flex-col">
-          <Typography variant="h5" className="uppercase">
-            Victims
-          </Typography>
-          <div>Total: <span className="font-bold">{dashboardData.victims}</span></div>
-        </div>
-        <div className="max-w-[250px]">
-          <Button content="View victims" onClick={() => router.push({ pathname: `${currentUser.type}/victims` })} />
-        </div>
-      </div>
-
-      <div className="shadow-md border-2 p-5 rounded-md flex flex-col gap-2 justify-between">
-        <div className="flex flex-col">
-          <Typography variant="h5" className="uppercase">
-            Criminals
-          </Typography>
-          <div>Total: <span className="font-bold">{dashboardData.criminals}</span></div>
-        </div>
-        <div className="max-w-[250px]">
-          <Button content="View criminals" onClick={() => router.push({ pathname: `${currentUser.type}/criminals` })} />
-        </div>
-      </div>
+      <DashboardItem total={dashboardData.victims} label="Victims" />
+      <DashboardItem total={dashboardData.criminals} label="Criminals" />
     </div>
 
-    <div className="shadow-md border-2 p-5 rounded-md flex flex-col justify-between">
-      <div className="flex flex-col gap-2">
-        <Typography variant="h5" className="uppercase">
-          Polices
-        </Typography>
-        <div>Total: <span className="font-bold">{totalPolices}</span></div>
-        <div>
-          <div className="flex flex-col gap-1">
-            <Typography variant="h6">Rank</Typography>
-            <DetailsList items={Object.entries(dashboardData.polices)} />
-          </div>
-        </div>
+    <DashboardItem label="Polices" total={totalPolices}>
+      <div className="flex flex-col gap-1">
+        <Typography variant="h6">Rank</Typography>
+        <DetailsList items={Object.entries(dashboardData.polices)} />
       </div>
-      <div className="max-w-[250px]">
-        <Button content="View polices" onClick={() => router.push({ pathname: `${currentUser.type}/polices` })} />
-      </div>
-    </div>
+    </DashboardItem>
 
-    <div className="shadow-md border-2 p-5 rounded-md flex flex-col justify-between">
-      <div className="flex flex-col gap-2">
-        <Typography variant="h5" className="uppercase">
-          Crimes
-        </Typography>
-        <div>Total: <span className="font-bold">{totalCasefiles}</span></div>
-        <div className="flex gap-10">
-          <div className="flex flex-col gap-1 min-w-[150px]">
-            <Typography variant="h6">Category</Typography>
-            <DetailsList items={Object.entries(dashboardData.crimes.categories)} />
-          </div>
-          <div className="flex flex-col gap-1 min-w-[250px]">
-            <Typography variant="h6">Weapon</Typography>
-            <DetailsList items={Object.entries(dashboardData.crimes.weapons)} />
-          </div>
-        </div>
+    <DashboardItem label="Crimes" total={totalCasefiles}>
+      <div className="flex flex-col gap-1 min-w-[150px]">
+        <Typography variant="h6">Category</Typography>
+        <DetailsList items={Object.entries(dashboardData.crimes.category)} />
       </div>
-      <div className="max-w-[250px]">
-        <Button content="View casefiles" onClick={() => router.push({ pathname: `${currentUser.type}/casefiles` })} />
+      <div className="flex flex-col gap-1 min-w-[250px]">
+        <Typography variant="h6">Weapon</Typography>
+        <DetailsList items={Object.entries(dashboardData.crimes.weapon)} />
       </div>
-    </div>
+    </DashboardItem>
   </div > : null
 }
