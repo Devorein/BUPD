965219aa295269cc92b5acd603229a8cb046a2d1
Safwan Shaheer <devorein00@gmail.com>
2022-01-07 14:58:40 +0600

    Fixed client build issues

--- packages/client/src/api/mutations/useDeleteCasefileMutation.ts
+++ packages/client/src/api/mutations/useDeleteCasefileMutation.ts
@@ -1,10 +1,10 @@
-import { DeleteCasefileResponse, ICasefile } from '@bupd/types';
+import { DeleteCasefileResponse, ICasefilePopulated } from '@bupd/types';
 import { useApiMutation } from '../../hooks';
 import { useGetCasefilesQueryData } from '../queries/useGetCasefilesQuery';
 import { useDeleteMutationCache } from '../utils/useDeleteMutationCache';
 
 export function useDeleteCasefileMutationCache() {
-	return useDeleteMutationCache<ICasefile, DeleteCasefileResponse>(
+	return useDeleteMutationCache<ICasefilePopulated, DeleteCasefileResponse>(
 		'casefile',
 		useGetCasefilesQueryData,
 		'case_no'
--- packages/client/src/api/mutations/useDeletePoliceMutation.ts
+++ packages/client/src/api/mutations/useDeletePoliceMutation.ts
@@ -1,10 +1,10 @@
-import { DeletePoliceResponse, IPolice } from '@bupd/types';
+import { DeletePoliceResponse, IPolicePopulated } from '@bupd/types';
 import { useApiMutation } from '../../hooks';
 import { useGetPolicesQueryData } from '../queries/useGetPolicesQuery';
 import { useDeleteMutationCache } from '../utils/useDeleteMutationCache';
 
 export function useDeletePoliceMutationCache() {
-	return useDeleteMutationCache<IPolice, DeletePoliceResponse>(
+	return useDeleteMutationCache<IPolicePopulated, DeletePoliceResponse>(
 		'police',
 		useGetPolicesQueryData,
 		'nid'
