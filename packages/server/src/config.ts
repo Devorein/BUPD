171672628c3d49ca6a37ab4bc3ceeefc4b11bcb8
Safwan Shaheer <devorein00@gmail.com>
2022-02-04 20:21:09 +0600

    Connected to database using sequelize

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -6,3 +6,11 @@ const envConfig = dotenv.parse(fs.readFileSync(path.resolve(__dirname, '../../..
 Object.keys(envConfig).forEach((key) => {
 	process.env[key] = envConfig[key];
 });
+
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD!;
+export const DATABASE_HOST = process.env.DATABASE_HOST!;
+export const SERVER_PORT = process.env.SERVER_PORT!;
+export const PASSWORD_SALT = process.env.PASSWORD_SALT!;
+export const JWT_SECRET = process.env.JWT_SECRET!;
+export const DATABASE_NAME = process.env.DATABASE_NAME!;
+export const DATABASE_USER = process.env.DATABASE_USER!;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,15 @@
 import cors from 'cors';
 import express from 'express';
-import './config';
+import { Sequelize } from 'sequelize';
+import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_USER, SERVER_PORT } from './config';
 import logger from './middlewares/logger';
 import { parseQueryTypes } from './middlewares/parseQueryTypes';
 import RootRouter from './routes';
-import pool from './utils/pool';
+
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
+  host: DATABASE_HOST,
+  dialect: "postgres"
+});
 
 const app = express();
 app.use(
@@ -18,12 +23,11 @@ app.use(express.json());
 app.use(logger);
 app.use('/v1', RootRouter);
 
-const PORT = process.env.SERVER_PORT;
-
-app.listen(PORT, async () => {
-	console.log(`Server listening on port ${PORT}`);
+app.listen(SERVER_PORT, async () => {
+	console.log(`Server listening on port ${SERVER_PORT}`);
 	try {
-		await pool.getConnection();
+    // Testing database connection
+    await sequelize.authenticate();
 		console.log(`Connected to database`);
 	} catch (err) {
 		console.error(`Error connecting to database\n${err.message}`);
